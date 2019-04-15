67dc0f6541c51ac0ee5f1ed904fa61b078e4640e
Anastasiia <anastasia.halk27@gmail.com>
2019-04-15 16:02:08 +0300

    tab on box

--- ARDemo/ARDemo/ViewController.swift
+++ ARDemo/ARDemo/ViewController.swift
@@ -17,11 +17,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
       
-        // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        sceneView.autoenablesDefaultLighting = true
         
         let scene = SCNScene()
-        createFigure(in: scene)
+        createBox(in: scene)
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(boxTapped(touch:)))
+        self.sceneView.addGestureRecognizer(gestureRecognizer)
+        
+        sceneView.scene = scene
+        
         
 //        let sphereGeometry = SCNSphere(radius: 0.1)
 //        let sphereMaterial = SCNMaterial()
@@ -62,25 +68,63 @@ class ViewController: UIViewController {
         
     }
     
-    private func createFigure(in scene: SCNScene) {
-        let array: [SCNGeometry] = [SCNPlane(), SCNSphere(), SCNBox(), SCNPyramid(), SCNTube(), SCNCone(), SCNTorus(), SCNCylinder(), SCNCapsule()]
-        var xCoordinate: Double = 1
+    @objc func boxTapped(touch: UITapGestureRecognizer) {
+        let sceneView = touch.view as! SCNView
+        let touchLocation = touch.location(in: sceneView)
         
-        sceneView.autoenablesDefaultLighting = true
+        let touchResults = sceneView.hitTest(touchLocation, options: [:])
         
-        for geometryShape in array {
-            let node = SCNNode(geometry: geometryShape)
-            
-            let material = SCNMaterial()
-            material.diffuse.contents = UIColor.red
-            
-            node.geometry?.materials = [material]
-            node.scale = SCNVector3(0.1, 0.1, 0.1)
-            node.position = SCNVector3(xCoordinate, 0, -1)
-            xCoordinate -= 0.2
-            
-            scene.rootNode.addChildNode(node)
+        guard !touchResults.isEmpty, let node = touchResults.first?.node else {
+            return
         }
+
+        let boxMaterial = SCNMaterial()
+        boxMaterial.diffuse.contents = UIColor.blue
+        boxMaterial.specular.contents = UIColor.red
+        node.geometry?.materials[0] = boxMaterial
+
+
+
+    }
+    
+    private func createBox(in scene: SCNScene) {
+        
+        let box = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
+        let boxMaterial = SCNMaterial()
+        
+        boxMaterial.diffuse.contents = UIColor.red
+        boxMaterial.specular.contents = UIColor.yellow
+        
+        let boxNode = SCNNode(geometry: box)
+        boxNode.geometry?.materials = [boxMaterial]
+        boxNode.position = SCNVector3(0.0, 0.0, -1.0)
+        scene.rootNode.addChildNode(boxNode)
+        
+        
+        
+        
+        
+        
+        
+        
+//        let array: [SCNGeometry] = [SCNPlane(), SCNSphere(), SCNBox(), SCNPyramid(), SCNTube(), SCNCone(), SCNTorus(), SCNCylinder(), SCNCapsule()]
+//        var xCoordinate: Double = 1
+//
+//        sceneView.autoenablesDefaultLighting = true
+//
+//        for geometryShape in array {
+//            let node = SCNNode(geometry: geometryShape)
+//
+//            let material = SCNMaterial()
+//            material.diffuse.contents = UIColor.red
+//
+//            node.geometry?.materials = [material]
+//            node.scale = SCNVector3(0.1, 0.1, 0.1)
+//            node.position = SCNVector3(xCoordinate, 0, -1)
+//            xCoordinate -= 0.2
+//
+//            scene.rootNode.addChildNode(node)
+//        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
