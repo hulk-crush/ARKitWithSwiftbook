17c19c79f254ec3c5302f80b682176acd0bba092
Anastasiia <anastasia.halk27@gmail.com>
2019-04-15 19:25:23 +0300

    placing objects on planes

---
+++ ARKitPlanes/ARKitPlanes/Box.swift
@@ -0,0 +1,35 @@
+//
+//  Box.swift
+//  ARKitPlanes
+//
+//  Created by Admin on 15/04/2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import SceneKit
+import ARKit
+
+class Box: SCNNode {
+    
+    init(atPosition position: SCNVector3) {
+        super.init()
+        
+        let boxGeometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.green
+        
+        boxGeometry.materials = [material]
+        
+        self.geometry = boxGeometry
+        
+        self.position = position
+        
+        
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- ARKitPlanes/ARKitPlanes/ViewController.swift
+++ ARKitPlanes/ARKitPlanes/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController{
 
     @IBOutlet var sceneView: ARSCNView!
+    
+    var planes = [Plane]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +24,45 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        
+        sceneView.autoenablesDefaultLighting = true
         
         // Create a new scene
             let scene = SCNScene()
         // Set the scene to the view
         sceneView.scene = scene
+        
+        setupGestures()
+    }
+    
+    func setupGestures() {
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(placeBox))
+        tapGestureRecognizer.numberOfTapsRequired = 1
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func placeBox(tapGesture: UITapGestureRecognizer) {
+    
+        let sceneView = tapGesture.view as! ARSCNView
+        let location = tapGesture.location(in: sceneView)
+        
+        let hitTestResult = sceneView.hitTest(location, types: .existingPlaneUsingExtent)
+        guard let hitResult = hitTestResult.first else { return }
+        
+        createBox(hitResult: hitResult)
+    }
+    
+    func createBox(hitResult: ARHitTestResult) {
+        
+        let position = SCNVector3(hitResult.worldTransform.columns.3.x,
+                                  hitResult.worldTransform.columns.3.y + 0.05,
+                                  hitResult.worldTransform.columns.3.z)
+        
+        let box = Box(atPosition: position)
+        sceneView.scene.rootNode.addChildNode(box)
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,6 +71,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
 
+        configuration.planeDetection = .horizontal
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -44,5 +81,32 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+}
+
+
+//MARK: -ARSCNViewDelegate
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        guard anchor is ARPlaneAnchor else { return }
+        
+        let plane = Plane(anchor: anchor as! ARPlaneAnchor)
+        
+        self.planes.append(plane)
+        node.addChildNode(plane)
+        
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        let plane = self.planes.filter { plane in
+            return plane.anchor.identifier == anchor.identifier
+        }.first
+        
+        guard plane != nil else {
+            return
+        }
+        
+        plane?.update(anchor: anchor as! ARPlaneAnchor)
     }
 }
